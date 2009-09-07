99bff842d2fd79aba92202d4fc45a3de96535f64
jpld <imjunpyo@gmail.com>
2009-09-07 13:55:02 -0700

    initial import of skeleton project

---
+++ CameraObscuraPlugIn.h
@@ -0,0 +1,22 @@
+//
+//  CameraObscuraPlugIn.h
+//  CameraObscura
+//
+//  Created by jpld on 07 Sept 2009.
+//  Copyright (c) 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <Quartz/Quartz.h>
+
+@interface CameraObscuraPlugIn : QCPlugIn
+{
+}
+
+/*
+Declare here the Obj-C 2.0 properties to be used as input and output ports for the plug-in e.g.
+@property double inputFoo;
+@property(assign) NSString* outputBar;
+You can access their values in the appropriate plug-in methods using self.inputFoo or self.inputBar
+*/
+
+@end
