abacb3636241614cf0d5b2f9201fcf0796f289b8
jpld <imjunpyo@gmail.com>
2009-09-07 16:05:10 -0700

    add dummy settings view controller to project

---
+++ CameraObscuraPlugInViewController.h
@@ -0,0 +1,13 @@
+//
+//  CameraObscuraPlugInViewController.h
+//  CameraObscura
+//
+//  Created by jpld on 07 Sept 2009.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <Quartz/Quartz.h>
+
+@interface CameraObscuraPlugInViewController : QCPlugInViewController {
+}
+@end
---
+++ CameraObscuraPlugInViewController.m
@@ -0,0 +1,24 @@
+//
+//  CameraObscuraPlugInViewController.m
+//  CameraObscura
+//
+//  Created by jpld on 07 Sept 2009.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import "CameraObscuraPlugInViewController.h"
+
+@implementation CameraObscuraPlugInViewController
+
+- (id)initWithPlugIn:(QCPlugIn*)plugIn viewNibName:(NSString*)name {
+    self = [super initWithPlugIn:plugIn viewNibName:name];
+    if (self) {
+    }
+    return self;
+}
+
+- (void)dealloc {
+    [super dealloc];
+}
+
+@end
