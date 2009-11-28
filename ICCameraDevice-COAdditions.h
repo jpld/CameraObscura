d94ae6aac0d8385e4b54e0acc4685f26020794a2
jpld <imjunpyo@gmail.com>
2009-11-27 22:31:34 -0800

    add empty files for camera device category

---
+++ ICCameraDevice-COAdditions.h
@@ -0,0 +1,12 @@
+//
+//  ICCameraDevice-COAdditions.h
+//  CameraObscura
+//
+//  Created by jpld on 27 Nov 2009.
+//  Copyright (c) 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ICCameraDevice_COAdditions : NSObject {}
+@end
---
+++ ICCameraDevice-COAdditions.m
@@ -0,0 +1,12 @@
+//
+//  ICCameraDevice-COAdditions.m
+//  CameraObscura
+//
+//  Created by jpld on 27 Nov 2009.
+//  Copyright (c) 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import "ICCameraDevice-COAdditions.h"
+
+@implementation ICCameraDevice_COAdditions
+@end
